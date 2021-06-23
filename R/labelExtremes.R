f7cf0e84613e5c184ebf6bd917cb9c3c483aa34f
Tejasvi Gupta <tejasvigupta723@gmail.com>
2021-06-24 03:11:31 +0530

    Modified the function to use 'getExtremeCoords'

--- R/labelExtremes.R
+++ R/labelExtremes.R
@@ -22,35 +22,18 @@
 #'
 #' @return None.  Annotates the plot with labels.
 #'
-#' @author Bryan A. Hanson, DePauw University.
+#' @author Bryan A. Hanson, DePauw University,Tejasvi Gupta.
 #'
 #' @keywords utilities
 #' @export
-#' @importFrom stats quantile
-#' @importFrom graphics text
 #' @noRd
 #'
 .labelExtremes <- function(data, names, tol) {
-  px <- data[, 1]
-  py <- data[, 2]
-  pl <- names
-  if (is.numeric(pl)) pl <- sprintf("%.2f", pl)
+  newList <- .getExtremeCoords(data, names, tol)
 
-  q.x <- quantile(px, probs = c(1.0 - tol, tol), na.rm = TRUE)
-  sel.x <- (px <= q.x[2]) | (px >= q.x[1])
-  keep.x <- subset(px, sel.x)
-  keep.x <- match(keep.x, px) # need to keep this & corresponding y
-
-  q.y <- quantile(py, probs = c(1.0 - tol, tol), na.rm = TRUE)
-  sel.y <- (py <= q.y[2]) | (py >= q.y[1])
-  keep.y <- subset(py, sel.y)
-  keep.y <- match(keep.y, py) # need to keep this & corresponding x
-
-  keep <- unique(c(keep.x, keep.y))
-
-  x <- px[keep]
-  y <- py[keep]
-  l <- pl[keep]
+  x <- newList$x
+  y <- newList$y
+  l <- newList$l
 
   for (n in c(1:length(x))) {
     text(x[n], y[n], l[n], pos = 4, offset = 0.2, cex = 0.5)
