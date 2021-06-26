b23c931acbb4a062df575234a0cb3e330b2a7d45
Bryan <hanson@depauw.edu>
2021-06-25 19:52:57 -0700

    bring over unit tests for chkGraphicsOptions & use roxut

--- R/chkGraphicsOpt.R
+++ R/chkGraphicsOpt.R
@@ -15,6 +15,22 @@
 #'
 #' @export
 #'
+#' @tests tinytest
+#' # Check for unset graphic mode
+#' expect_equal(chkGraphicsOpt(), "base")
+#' 
+#' # check for 'base'
+#' options(ChemoSpecGraphics = "base")
+#' expect_equal(chkGraphicsOpt(), "base")
+#' 
+#' # check for 'ggplot2'
+#' options(ChemoSpecGraphics = "ggplot2")
+#' expect_equal(chkGraphicsOpt(), "ggplot2")
+#' 
+#' # check for invalid mode
+#' options(ChemoSpecGraphics = "xyz")
+#' expect_equal(chkGraphicsOpt(), "base")
+
 chkGraphicsOpt <- function(silent = TRUE) {
   go <- getOption("ChemoSpecGraphics")
   valid <- c("base", "ggplot2", "plotly", "shiny")
---
+++ inst/tinytest/test_chkGraphicsOpt.R
@@ -0,0 +1,16 @@
+# File created by roxut; edit the function definition file, not this file
+ 
+# Check for unset graphic mode
+expect_equal(chkGraphicsOpt(), "base")
+
+# check for 'base'
+options(ChemoSpecGraphics = "base")
+expect_equal(chkGraphicsOpt(), "base")
+
+# check for 'ggplot2'
+options(ChemoSpecGraphics = "ggplot2")
+expect_equal(chkGraphicsOpt(), "ggplot2")
+
+# check for invalid mode
+options(ChemoSpecGraphics = "xyz")
+expect_equal(chkGraphicsOpt(), "base")
