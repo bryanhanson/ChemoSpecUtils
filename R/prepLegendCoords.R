0990a051e978a0094f2fda692bbd7269c36703bb
Tejasvi Gupta <tejasvigupta723@gmail.com>
2021-08-05 18:51:15 +0530

    Modified prepLegendCoords

--- R/prepLegendCoords.R
+++ R/prepLegendCoords.R
@@ -1,13 +1,55 @@
-prepLegendCoords<- function(spectra,leg.loc,y.min,y.max,x.min,x.max) {
+prepLegendCoords<- function(leg.loc) {
 if (is.list(leg.loc))
 {
-  leg.loc$x <- (leg.loc$x) * (x.max - x.min) + x.min
-  leg.loc$y <- (leg.loc$y) * (y.max - y.min) + y.min
-  return(leg.loc) 
+  if ( exists('x' , where=leg.loc) && (exists('y' ,where=leg.loc)) )
+  {
+    return(leg.loc)
+  }
 }
   else
   {
-    return(leg.loc)
+    if (leg.loc == "topright") {
+      lab.x <- 0.9
+      lab.y <- 0.9
+    }
+    if (leg.loc == "topleft") {
+      lab.x <- 0.01
+      lab.y <- 0.9
+    }
+    if (leg.loc == "bottomright") {
+      lab.x <- 0.9
+      lab.y <- 0.2
+    }
+    if (leg.loc == "bottomleft") {
+      lab.x <- 0.01
+      lab.y <- 0.2
+    }
+    if (leg.loc == "bottom") {
+      lab.x <- 0.45
+      lab.y <- 0.2
+    }
+    
+    if (leg.loc == "top") {
+      lab.x <- 0.45
+      lab.y <- 0.9
+    }
+    
+    if (leg.loc == "left") {
+      lab.x <- 0.01
+      lab.y <- 0.5
+    }
+    
+    if (leg.loc == "right") {
+      lab.x <- 0.9
+      lab.y <- 0.5
+    }
+    
+    if (leg.loc == "middle") {
+      lab.x <- 0.45
+      lab.y <- 0.5
+    }
+     leg.loc=list(x=lab.x,y=lab.y) 
+     return(leg.loc)
   }
 }
 
