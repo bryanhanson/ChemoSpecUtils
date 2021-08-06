cdfcd8fdf7aa1dd1618ccb9f64c8334cd7656eba
Tejasvi Gupta <tejasvigupta723@gmail.com>
2021-08-06 23:42:08 +0530

    Added documentation for prepLegendCoords

--- R/prepLegendCoords.R
+++ R/prepLegendCoords.R
@@ -1,10 +1,24 @@
-prepLegendCoords <- function(go, leg.loc, x.min = 0, x.max = 0, y.min = 0, y.max = 0) {
-  if (is.list(leg.loc) && go == "ggplot2") { #case when a list is passed in ggplot2 mode
+#'
+#' Add Points to a Plot
+#'
+#' @noRd
+#' @param go A string specifying the graphics mode
+#' @param leg.loc A list giving x and y coordinates
+#' @param x.min A number giving the minimum x value in the lot. It is used only if graphics mode is set to base.
+#' @param x.max A number giving the maximum x value in the plot. It is used only if graphics mode is set to base.
+#' @param y.min A number giving the minimum y value in the plot. It is used only if graphics mode is set to base.
+#' @param y.max A number giving the maximum y value in the plot. It is used only if graphics mode is set to base.
+#'
+#' @return A list of coordinates specifying x and y position of the legend, except in one case where a string is returned.
+#' @export
+#'
+.prepLegendCoords <- function(go, leg.loc, x.min = 0.0, x.max = 0.0, y.min = 0.0, y.max = 0.0) {
+  if (is.list(leg.loc) && go == "ggplot2") { # case when a list is passed in ggplot2 mode
     if (exists("x", where = leg.loc) && (exists("y", where = leg.loc))) {
       return(leg.loc)
     }
   } else if (is.list(leg.loc) && go == "base") { # case when list is passed in base mode
-    if (exists("x", where = leg.loc) && (exists("y", where = leg.loc))) {
+    if (exists("x", where = leg.loc) && (exists("y", where = leg.loc))) { # Converting NPC coordinates are  to data coordinates
       lab.x <- (leg.loc$x) * (x.max - x.min) + x.min
       lab.y <- (leg.loc$y) * (y.max - y.min) + y.min
       leg.loc <- list(x = lab.x, y = lab.y)
@@ -54,6 +68,9 @@ prepLegendCoords <- function(go, leg.loc, x.min = 0, x.max = 0, y.min = 0, y.max
     leg.loc <- list(x = lab.x, y = lab.y)
     return(leg.loc)
   } else { # case when a string is passed in base mode
+    if (!is.character(leg.loc)) {
+      stop("Expected a character string for leg.loc")
+    }
     return(leg.loc)
   }
 }
