f0afb7f1783a27eee51a0864b4eebe3bda5600df
Bryan <hanson@depauw.edu>
2021-08-25 12:42:57 -0700

    review & style ggAddLegend

--- R/ggAddLegend.R
+++ R/ggAddLegend.R
@@ -1,18 +1,19 @@
 #'
-#' Title
+#' Add Legend to a ggplot2 Plot
 #'
-#' @noRd
-#' @param go A string specifying the graphics mode
+#' @param go A string specifying the graphics mode.
 #' @param spectra An object of S3 class \code{\link{Spectra}}.
-#' @param use.sym A bool specifying if the symbols will be used or not.
-#' @param leg.loc A list giving x and y coordinates
-#' @param p ggplot on which the legend is being added on.
+#' @param use.sym Logical specifying if symbols will be used or not.
+#' @param leg.loc A list giving x and y coordinates.
+#' @param p ggplot to which the legend will be added.
 #'
 #' @return A ggplot with custom legend.
+#'
 #' @export
+#' @noRd
 #' @importFrom grid grobTree textGrob gpar
 #'
-.ggAddLegend <- function(go,spectra,use.sym,leg.loc,p ) {
+.ggAddLegend <- function(go, spectra, use.sym, leg.loc, p) {
   group <- c(NA_real_)
   color <- c(NA_real_)
   for (i in spectra$groups) {
@@ -20,7 +21,7 @@
       group <- c(group, i)
     }
   }
-  
+
   for (i in spectra$colors) {
     if (!(i %in% color)) {
       color <- c(color, i)
@@ -28,25 +29,25 @@
   }
   group <- group[-1]
   color <- color[-1]
-  
+
   # If use.sym then color of the legend should be black
   if (use.sym) {
     color <- rep("black", length(group))
   }
-  
+
   leg.loc <- .prepLegendCoords(go, leg.loc)
   lab.x <- leg.loc$x
   lab.y <- leg.loc$y
-  gap<-0.04
+  gap <- 0.04
   keys <- grobTree(textGrob("Key",
-                            x = lab.x, y = lab.y + gap, hjust = 0,
-                            gp = gpar(col = "black", fontsize = 10)
+    x = lab.x, y = lab.y + gap, hjust = 0,
+    gp = gpar(col = "black", fontsize = 10)
   ))
-  
+
   for (i in 1:length(group)) {
     grob <- grid::grobTree(textGrob(group[i],
-                                    x = lab.x, y = lab.y, hjust = 0,
-                                    gp = gpar(col = color[i], fontsize = 10)
+      x = lab.x, y = lab.y, hjust = 0,
+      gp = gpar(col = color[i], fontsize = 10)
     ))
     lab.y <- lab.y - gap
     p <- p + annotation_custom(grob) + annotation_custom(keys)
