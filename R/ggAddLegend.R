f371b4d0acd31cc7f57e6b783919a9326c50dff2
Bryan <hanson@depauw.edu>
2021-08-31 18:53:31 -0700

    add symbol grobs to .ggAddLegend

--- R/ggAddLegend.R
+++ R/ggAddLegend.R
@@ -1,7 +1,7 @@
 #'
 #' Add Legend to a ggplot2 Plot
 #'
-#' This function creates a suitable legend and returns it as a grob,
+#' This function creates a suitable legend and returns it as a set of grobs,
 #' ready to be added to an existing plot.
 #'
 #' @param spectra An object of S3 class \code{\link{Spectra}}.
@@ -20,29 +20,40 @@
   gr_sum <- sumGroups(spectra) # group, color, symbol
   group <- gr_sum$group
   color <- gr_sum$color
-  if (use.sym) color <- rep("black", nrow(gr_sum))
   symbol <- gr_sum$symbol
   ng <- length(group)
 
-  # figure out legend coords
+  # figure out legend coords; reference is top left
   leg.loc <- .prepLegendCoords("ggplot2", leg.loc)
   leg.x <- leg.loc$x
   leg.y <- leg.loc$y
 
   # create the pieces on the fly
   # position pieces relative to "Key" at top
-  grobs_list <- vector("list", ng + 1) # list to store the grobs temporarily
+  if (!use.sym) grobs_list <- vector("list", ng + 1) # group name + title
+  if (use.sym) grobs_list <- vector("list", ng*2 + 1) # group symbol, group name + title
   y.off <- 0.035
+  x.off <- 0.025
 
   key_grob <- textGrob("Key", x = leg.x, y = leg.y, just = "left",
     gp = gpar(col = "black", fontsize = 10))
   grobs_list[[1]] <- key_grob
 
   if (!use.sym) {
-    for (i in 1:length(group)) {
+    for (i in 1:ng) {
       leg.y <- leg.y - y.off # descend a bit
       grobs_list[[i + 1]] <- textGrob(group[i], x = leg.x, y = leg.y, just = "left",
         gp = gpar(col = color[i], fontsize = 10))
+    }
+  }
+
+  if (use.sym) {
+    color <- rep("black", ng)
+    for (i in 1:ng) {
+      leg.y <- leg.y - y.off # descend a bit
+      grobs_list[[i + 1]] <- textGrob(group[i], x = leg.x + x.off, y = leg.y, just = "left",
+        gp = gpar(col = color[i], fontsize = 10))
+      grobs_list[[i + 1 + ng]] <- pointsGrob(symbol[i], x = leg.x , y = leg.y, size = unit(0.5, "char"))
     }
   }
 
