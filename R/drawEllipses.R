433bcfb1af69bdb30efeb36050bda94534f6d478
Bryan Hanson <hanson@depauw.edu>
2022-01-16 21:51:47 -0700

    eliminate plyr from .drawEllipses

--- R/drawEllipses.R
+++ R/drawEllipses.R
@@ -5,34 +5,38 @@
 #' @export
 #' @noRd
 #' @importFrom graphics lines
-#' @importFrom plyr dlply llply m_ply
-#
+#'
 
 .drawEllipses <- function(ELL, gr, ellipse, use.sym, ...) {
-  if (ellipse == "cls") {
-    cls.coords <- llply(ELL, function(x) {
-      x[1:2]
-    })
-    cls.coords <- llply(cls.coords, function(x) {
-      do.call(cbind, x)
-    })
-    if (!use.sym) m_ply(cbind(x = cls.coords, col = gr$color, lty = 3), lines, ...)
-    if (use.sym) m_ply(cbind(x = cls.coords, col = "black", lty = 3), lines, ...)
-  }
-
-  if (ellipse == "rob") {
-    rob.coords <- llply(ELL, function(x) {
-      x[4:5]
-    })
-    rob.coords <- llply(rob.coords, function(x) {
-      do.call(cbind, x)
-    })
-    if (!use.sym) m_ply(cbind(x = rob.coords, col = gr$color), lines, ...)
-    if (use.sym) m_ply(cbind(x = rob.coords, col = "black"), lines, ...)
-  }
 
   if (ellipse == "both") {
     .drawEllipses(ELL, gr, ellipse = "cls", use.sym, ...)
     .drawEllipses(ELL, gr, ellipse = "rob", use.sym, ...)
+    return()
+  }
+
+  # Prepare the coordinates
+
+  if (ellipse == "cls") {
+    coords <- lapply(ELL, function(x) { x[1:2] })
+    coords <- lapply(coords, function(x) { do.call(cbind, x) })
+    ltype <- 3L
+  }
+
+  if (ellipse == "rob") {
+    coords <- lapply(ELL, function(x) { x[4:5] })
+    coords <- lapply(coords, function(x) { do.call(cbind, x) })
+    ltype <- 1L
+  }
+
+  ellpts <- lapply(coords, function(x) { cbind(x = x[,1], y = x[,2]) })
+
+  # Draw the lines
+  # note: hate to loop over list elements, but don't see a way to use
+  # lapply and access gr$color
+  
+  for (i in 1:length(ellpts)) {
+    if (use.sym) lines(ellpts[[i]], lty = ltype, col = "black", ...)
+    if (!use.sym) lines(ellpts[[i]], lty = ltype, col = gr$color[i], ...)
   }
 }
